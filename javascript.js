ad08823c2d77433d0d6726358e97e6c89bd3d832
Harihar Prasad <137610971+HariharPrasadd@users.noreply.github.com>
2025-03-02 16:22:44 +0530

    Implemented all basic functionality

--- javascript.js
+++ javascript.js
@@ -4,24 +4,31 @@ const displayDimensions = document.querySelector(".dimensions");
 
 let dimension = slider.value;
 
-displayDimensions.textContent = dimension;
+displayDimensions.textContent = `${dimension} × ${dimension}`;
 
-let padWidth = 500;
-let padHeight = 500;
+let padWidth = 600;
+let padHeight = 600;
 
 pad.setAttribute("style", `height: ${padHeight}px; width: ${padWidth}px`);
 
 
 for(let i = 0; i < dimension*dimension; i++){
     const gridbox = document.createElement("div");
-    gridbox.setAttribute("style", `height: ${padHeight/dimension}px; width: ${padWidth/dimension}px`);
+    gridbox.setAttribute("style", 
+        `height: ${padHeight/dimension}px; 
+        width: ${padWidth/dimension}px;`);
     gridbox.classList.add("gridbox");
+    gridbox.addEventListener("mouseenter", (e) => {normalMouseEnter(e)});
     pad.appendChild(gridbox);
+}
+
+function normalMouseEnter(event){
+    event.target.style.backgroundColor = "black";
 }
 
 slider.oninput = function() {
     dimension = this.value;
-    displayDimensions.textContent = dimension;
+    displayDimensions.textContent = `${dimension} × ${dimension}`;
 
     pad.textContent = '';
 
@@ -29,6 +36,8 @@ slider.oninput = function() {
         const gridbox = document.createElement("div");
         gridbox.setAttribute("style", `height: ${padHeight/dimension}px; width: ${padWidth/dimension}px`);
         gridbox.classList.add("gridbox");
+        gridbox.addEventListener("mouseenter", (e) => {normalMouseEnter(e)});
         pad.appendChild(gridbox);
     }
 }
+ 
